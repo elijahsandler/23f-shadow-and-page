c2fb474474b8dee6a285b8bd7c87eafd728f0cde
elijahsandler <sandler.e@northeastern.edu>
2024-04-15 17:33:31 -0400

    praise be to cole palmer

--- flask-app/src/inventory/inventory.py
+++ flask-app/src/inventory/inventory.py
@@ -73,79 +73,84 @@ def get_copy_curses(copyID):
     the_response.mimetype = 'application/json'
     return the_response
 
+@inventory.route('/inventory/<copyID>/price', methods=['GET'])
+def get_copy_pricess(copyID):
+    cursor = db.get_db().cursor()
+    cursor.execute(
+        f'SELECT DateSet, Price \
+        FROM Inventory \
+        NATURAL JOIN BookPrices
+        WHERE CopyID={copyID} \
+        ORDER BY DateSet DESC')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
 ############################################################
 
-# # add a book to the db
-# @books.route('/books', methods=['POST'])
-# def add_new_book():
+# add a book to the db
+@inventory.route('/inventory', methods=['POST'])
+def add_new_copy():
     
-#     # collecting data from the request object 
-#     the_data = request.json
-#     current_app.logger.info(the_data)
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
 
-#     #extracting the variable
-#     book_id = the_data['book_id']
-#     title = the_data['book_title']
-#     year = the_data['book_year']
-#     firstname = the_data['book_authorfirstname']
-#     lastname = the_data['book_authorlastname']
-#     genre_id = the_data['genre_id']
-#     publisher_id = the_data['publisher_id']
+    #extracting the variable
+    book_id = the_data['book_id']
 
-#     # # Constructing the query
-#     query = f"insert into books (bookid, title, year, authorfirstname, authorlastname, \
-#         genreid, publisherid) values ('{book_id}', '{title}', '{year}', '{firstname}', '{lastname}', \
-#         '{genre_id}', '{publisher_id}')"
+    # # Constructing the query
+    query = f"insert into inventory (BookID) \
+        values ('{book_id}')"
 
-#     # executing and committing the insert statement 
-#     cursor = db.get_db().cursor()
-#     cursor.execute(query)
-#     db.get_db().commit()
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
     
-#     return 'Success!'
+    return 'Success!'
 
-# @books.route('/books', methods=['PUT'])
-# def update_book():
-#     # collecting data from the request object 
-#     the_data = request.json
-#     current_app.logger.info(the_data)
+@inventory.route('/inventory', methods=['PUT'])
+def update_copy():
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
 
-#     #extracting the variable
-#     book_id = the_data['book_id']
-#     title = the_data['book_title']
-#     year = the_data['book_year']
-#     firstname = the_data['book_authorfirstname']
-#     lastname = the_data['book_authorlastname']
-#     genre_id = the_data['genre_id']
-#     publisher_id = the_data['publisher_id']
+    #extracting the variable
+    book_id = the_data['book_id']
+    copy_id = the_data['copy_id']
 
-#     # Constructing the query
-#     query = f'update books set `GenreID` = "{genre_id}", `Title` = "{title}",\
-#           `Year` = {year}, `AuthorFirstName` = "{firstname}", `AuthorLastName` = "{lastname}", \
-#             `PublisherID` = "{publisher_id}" where `BookID` = {book_id}'
-#     current_app.logger.info(query)
+    # Constructing the query
+    query = f'update inventory set `BookID` = "{book_id}" where `CopyID` = {copy_id}'
+    current_app.logger.info(query)
 
-#     # executing and committing the update statement 
-#     cursor = db.get_db().cursor()
-#     cursor.execute(query)
-#     db.get_db().commit()
+    # executing and committing the update statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
     
-#     return 'Success!'
+    return 'Success!'
 
-# # remove a book from the database
-# @books.route('/books', methods=['DELETE'])
-# def remove_book():
-#     the_data = request.json
-#     current_app.logger.info(the_data)
+# remove a copy from the database
+@inventory.route('/inventory', methods=['DELETE'])
+def remove_copy():
+    the_data = request.json
+    current_app.logger.info(the_data)
 
-#     book_id = the_data['book_id']
+    copy_id = the_data['copy_id']
 
-#     query = f'delete from books where `BookID`={book_id}'
-#     current_app.logger.info(query)
+    query = f'delete from books where `CopyID`={copy_id}'
+    current_app.logger.info(query)
 
-#     # executing and committing the update statement 
-#     cursor = db.get_db().cursor()
-#     cursor.execute(query)
-#     db.get_db().commit()
+    # executing and committing the update statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
     
-#     return 'Success!'
+    return 'Success!'
