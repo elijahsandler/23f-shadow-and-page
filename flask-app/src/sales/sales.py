8a15edea9278abe90bd264a67a9e49797111d1b4
awesome20188 <sun.andre@northeastern.edu>
2024-04-17 14:54:48 -0400

    updated sales file

--- flask-app/src/sales/sales.py
+++ flask-app/src/sales/sales.py
@@ -2,8 +2,6 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
-
-
 sales = Blueprint('sales', __name__)
 
 # Get all sales from the DB
@@ -12,8 +10,8 @@ sales = Blueprint('sales', __name__)
 def get_sales():
     cursor = db.get_db().cursor()
     cursor.execute(
-        'SELECT SaleID, Employee \
-        From Sales;')
+        'SELECT * \
+        From Sales Order By SaleID;')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -23,5 +21,40 @@ def get_sales():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
+@sales.route('/sales/<saleID>', methods=['GET'])
+def get_sale(saleID):
+    cursor = db.get_db().cursor()
+    cursor.execute(f"select * from Sales where SaleID = {saleID}")
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+
+@sales.route('/top-sales', methods=['GET'])
+def get_top_x_sales():
+    cursor = db.get_db().cursor()
+    cursor.execute(f"SELECT E.FirstName, E.LastName, E.Position, E.EmployeeID, COUNT(*) AS total_sales From Sales as S \
+                   JOIN Employees as E ON E.EmployeeID = S.Employee GROUP BY E.employeeID \
+                   ORDER BY COUNT(*) DESC")
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+    
+
 
 
