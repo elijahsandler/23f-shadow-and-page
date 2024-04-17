6687436eed1959a70141586d5ec351834a0200bc
Sasha <sasha@Sashas-MacBook-Pro.local>
2024-04-17 10:41:38 -0400

    is it working???

--- flask-app/src/curses/curses.py
+++ flask-app/src/curses/curses.py
@@ -111,11 +111,27 @@ def get_curses_map():
     the_response.mimetype = 'application/json'
     return the_response
 
-counter_curses = Blueprint('counter_curses', __name__)
 
 # Get all counter curses inventory from the DB
-@counter_curses.route('/counter_curses', methods=['GET'])
+@curses.route('/counter-curses', methods=['GET'])
 def get_counter_curses():
+    cursor = db.get_db().cursor()
+    cursor.execute(
+        'SELECT * \
+        FROM countercurses')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+@curses.route('/counter-curses-count', methods=['GET'])
+def get_counter_curses_count():
     cursor = db.get_db().cursor()
     cursor.execute(
         'SELECT counter_curses.Name, COUNT(counter_curses.CounterID),\
